9191894602b9a4a838e68bad486c358001cad732
tensor-14 <saurabhsingh007ind@gmail.com>
2025-07-07 12:31:25 +0530

    chore(misc): some untracked files

--- DataArchival.Core/obj/Debug/net9.0/DataArchival.Core.AssemblyInfo.cs
+++ DataArchival.Core/obj/Debug/net9.0/DataArchival.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataArchival.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918cdbed1b1af72dcf0536c6dfaaa2392b97124b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataArchival.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataArchival.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
