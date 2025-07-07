dfe9b52a1745db5cc399cc69c1bccb0682d09732
tensor-14 <saurabhsingh007ind@gmail.com>
2025-07-07 12:38:39 +0530

    feat(deployment): add docker file

--- DataArchival.App/obj/Debug/net9.0/DataArchival.App.AssemblyInfo.cs
+++ DataArchival.App/obj/Debug/net9.0/DataArchival.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataArchival.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918cdbed1b1af72dcf0536c6dfaaa2392b97124b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9191894602b9a4a838e68bad486c358001cad732")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataArchival.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataArchival.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
